bdb8cfa9e3ef85247d309915822a72f059b694c8
Dorian J. Feistel <dfeistel3@gatech.edu>
2022-05-25 17:42:20 -0400

    renaming script

--- 00b_Python/print_stats.py
+++ 00b_Python/print_stats.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python3
 
 '''
--------------------------------------------
-Author :: Dorian J Feistel
-Email :: dfeistel3@gatech.edu
-GitHub :: https://github.com/dfesitel3
-Date Created :: May 2020
-License :: GNU GPLv3
-Copyright 2022 Dorian J Feistel
-All rights reserved
--------------------------------------------
-This script will calculating total data points, precision, recall, F1-score and accuracy and print to stdout
 
-usage:
-python3 stats_ANI.py <Full_ANI.ani> <ANI_min_value>
+this script if for calculating the precision and recall of sequence types (STs) using ANI values
+it will total data points, precision, recall, F1-score and accuracy
+
+stats_ANI.py <parsed_ANI_file> <ANI_value> > <output_file>
+
+Note: output is printed to stdout!
+
 '''
+
 import sys
 
 def getANI(in_ani):
-    '''read full ANI file and store as a dict'''
+    '''read the ANI fiel and store are a list (probably not the most memory efficient!)'''
     ST_dict = dict()
     fh = open(in_ani, 'r')
     next(fh) #skip header
-    '''create dict with query ST as key and line as values'''
     for line in fh:
         line = line.strip().split()
-        ANI = line[2]
-        query = line[5]
-        reference = line[6]
-        if query not in ST_dict:
-            ST_dict[query] = [[reference, ANI]]
+        if line[5] not in ST_dict:
+            ST_dict[line[5]] = [line]
         else:
-            ST_dict[query].append([reference, ANI])
+            ST_dict[line[5]].append(line)
     return ST_dict
 
 def get_table(ST_dict, ANI_min):
@@ -44,23 +36,21 @@ def get_table(ST_dict, ANI_min):
         FN = 0
         FP = 0
         for ele in ST_dict[ST]:
-            ST_compare = ele[0]
-            ANI = ele[1]
-            if ST_compare == ST and float(ANI) >= ANI_min:
+            if ele[6] == ST and float(ele[2]) > ANI_min:
                 TP += 1
-            elif ST_compare != ST and float(ANI) < ANI_min:
+            elif ele[6] != ST and float(ele[2]) < ANI_min:
                 TN += 1
-            elif ST_compare != ST and float(ANI) >= ANI_min:
+            elif ele[6] != ST and float(ele[2]) > ANI_min:
                 FP += 1
-            elif ST_compare == ST and float(ANI) < ANI_min:
+            elif ele[6] == ST and float(ele[2]) < ANI_min:
                 FN += 1
         table_dict[ST] = [TP, TN, FP, FN]
     return table_dict
 
-def calulate_stats(table_dict, ANI_min, ST_dict):
+def calulate_stats(table_dict, ANI_min):
     '''calulate precision and recall for a given ST'''
-    #print(f'Precision and recall for a given ST at a minimum ANI of {ANI_min}')
-    #print(f'ST\tData-points\tPrecision\tRecall\tF1-Score\tAccuracy')
+    print(f'Precision and recall for a given ST at a minimum ANI of {ANI_min}')
+    print(f'ST\tData-points\tPrecision\tRecall\tF1-Score\tAccuracy')
     ST_keys = table_dict.keys()
     for ST in ST_keys:
         '''[TP, TN, FP, FN]'''
@@ -69,13 +59,12 @@ def calulate_stats(table_dict, ANI_min, ST_dict):
         TN = confusion_matrix[1]
         FP = confusion_matrix[2]
         FN = confusion_matrix[3]
-        total_data = len(ST_dict[ST])
+        total_data = TP + TN + FP + FN
         Precision = round(TP / (TP + FP) * 100, 2)
         Recall = round(TP / (TP + FN) * 100, 2)
         F1_Score = round((2 * Precision * Recall) / (Precision + Recall), 2)
         Accuracy = round((TP + TN) / (TP + TN + FN + FP) * 100, 2)
         print(f'{ST}\t{total_data}\t{Precision}\t{Recall}\t{F1_Score}\t{Accuracy}')
-        #print(f'{TP}\t{TN}\t{FP}\t{FN}')
     return None
 
 if __name__ == "__main__":
@@ -84,4 +73,4 @@ if __name__ == "__main__":
     ANI_min = float(sys.argv[2])
     ST_dict = getANI(ANI_infile)
     table_dict = get_table(ST_dict, ANI_min)
-    calulate_stats(table_dict, ANI_min, ST_dict)
+    calulate_stats(table_dict, ANI_min)
